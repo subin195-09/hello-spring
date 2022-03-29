89d8392baf7ae08049420d7a34caea2e43ef3437
subin195-09 <k_subin@naver.com>
2022-03-29 23:31:25 +0900

    3/29 spring study start

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,49 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "skim!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http body 부분에 이 부분을 직접 넣어 주겠다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name; // view 없이 문자 그대로 내려감
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApit(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
