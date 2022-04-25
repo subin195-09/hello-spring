cb11a59f73e3195d390dcd595387b2ca29200d3c
subin195-09 <k_subin@naver.com>
2022-04-25 22:56:28 +0900

    4/25 zerok study start

---
+++ ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -0,0 +1,44 @@
+package org.zerock.ex2.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.ex2.entity.Memo;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MemoRepositoryTests {
+
+    @Autowired
+    MemoRepository memoRepository;
+
+    @Test
+    public void testClass() {
+        System.out.println(memoRepository.getClass().getName());
+    }
+
+    @Test
+    public void testInsertDummies() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Memo memo = Memo.builder().memoText("Sample..." + i).build();
+            memoRepository.save(memo);
+        });
+    }
+
+    @Test
+    public void testSelect() {
+        Long mno = 100L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+
+        System.out.println("=========================");
+        if (result.isPresent()) {
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
+    }
+
+    
+}
---
+++ inflearn/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,48 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.*;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+//    private DataSource dataSource;
+//
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+//
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
+}
