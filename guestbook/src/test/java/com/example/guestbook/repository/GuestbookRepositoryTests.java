792e0c41ccabb0bcace577c338ab2434d9399b6e
subin195-09 <k_subin@naver.com>
2022-05-03 02:03:18 +0900

    5/3 Querydsl test

--- guestbook/src/test/java/com/example/guestbook/repository/GuestbookRepositoryTests.java
+++ guestbook/src/test/java/com/example/guestbook/repository/GuestbookRepositoryTests.java
@@ -1,9 +1,16 @@
 package com.example.guestbook.repository;
 
 import com.example.guestbook.entity.Guestbook;
+import com.example.guestbook.entity.QGuestbook;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -38,5 +45,53 @@ public class GuestbookRepositoryTests {
 
 			guestbookRepository.save(guestbook);
 		}
+	}
+
+	@Test
+	public void testQuery1() {
+		Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+
+		QGuestbook qGuestbook = QGuestbook.guestbook; // 1
+
+		String keyword = "1";
+
+		BooleanBuilder builder = new BooleanBuilder(); // 2
+
+		BooleanExpression expression = qGuestbook.title.contains(keyword); // 3
+
+		builder.and(expression); // 4
+
+		Page<Guestbook> result = guestbookRepository.findAll(builder, pageable); // 5
+
+		result.stream().forEach(guestbook -> {
+			System.out.println(guestbook);
+		});
+	}
+
+	@Test
+	public void testQuery2() {
+		Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+
+		QGuestbook qGuestbook = QGuestbook.guestbook;
+
+		String keyword = "1";
+
+		BooleanBuilder builder = new BooleanBuilder();
+
+		BooleanExpression exTitle = qGuestbook.title.contains(keyword); // 1
+
+		BooleanExpression exContent = qGuestbook.content.contains(keyword); // 2
+
+		BooleanExpression exAll = exTitle.or(exContent); // 3
+
+		builder.and(exAll);
+
+		builder.and(qGuestbook.gno.gt(0L)); // 4
+
+		Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+		result.stream().forEach(guestbook -> {
+			System.out.println(guestbook);
+		});
 	}
 }
