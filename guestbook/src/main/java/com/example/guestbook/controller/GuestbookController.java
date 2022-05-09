f7cddec928b6e7cd6c56e3b21826be2350b4bad7
subin195-09 <k_subin@naver.com>
2022-05-09 23:54:43 +0900

    5/9 register controller add

--- guestbook/src/main/java/com/example/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/com/example/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package com.example.guestbook.controller;
 
+import com.example.guestbook.dto.GuestbookDTO;
 import com.example.guestbook.dto.PageRequestDTO;
 import com.example.guestbook.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -27,5 +30,22 @@ public class GuestbookController {
         log.info("list................" + pageRequestDTO);
 
         model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto..." + dto);
+
+        // 새로 추가된 엔티티의 번호
+        Long gno = service.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
     }
 }
