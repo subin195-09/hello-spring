7f5fce1302e59c1cf24db8c0b73395796cf91a54
subin195-09 <k_subin@naver.com>
2022-05-16 02:47:42 +0900

    5/16 guestbook delete add

--- guestbook/src/main/java/com/example/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/com/example/guestbook/controller/GuestbookController.java
@@ -50,7 +50,8 @@ public class GuestbookController {
         return "redirect:/guestbook/list";
     }
 
-    @GetMapping("/read")
+    // @GetMapping("/read")
+    @GetMapping({"/read", "/modify"})
     public void read(long gno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model) {
         log.info("gno : " + gno);
 
