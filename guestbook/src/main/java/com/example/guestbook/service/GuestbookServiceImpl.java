419f3939b4ccfef62066b07ce8a55d72419dec94
subin195-09 <k_subin@naver.com>
2022-05-19 03:59:38 +0900

    5/19 seach start

--- guestbook/src/main/java/com/example/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/com/example/guestbook/service/GuestbookServiceImpl.java
@@ -4,7 +4,10 @@ import com.example.guestbook.dto.GuestbookDTO;
 import com.example.guestbook.dto.PageRequestDTO;
 import com.example.guestbook.dto.PageResultDTO;
 import com.example.guestbook.entity.Guestbook;
+import com.example.guestbook.entity.QGuestbook;
 import com.example.guestbook.repository.GuestbookRepository;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
@@ -74,5 +77,47 @@ public class GuestbookServiceImpl implements GuestbookService{
 
             repository.save(entity);
         }
+    }
+
+    private BooleanBuilder getSearch(PageRequestDTO requestDTO) {
+        String type = requestDTO.getType();
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        String keyword = requestDTO.getKeyword();
+        BooleanExpression expression = qGuestbook.gno.gt(0L); // gno 가 양수일 때 조건만 생성
+        booleanBuilder.and(expression);
+
+        if (type == null || type.trim().length() == 0) { // 검색 조간이 없는 경우
+            return booleanBuilder;
+        }
+
+        BooleanBuilder conditionBuilder = new BooleanBuilder();
+
+        if (type.contains("t")) { // title
+            conditionBuilder.or(qGuestbook.title.contains(keyword));
+        }
+        if (type.contains("c")) { // content
+            conditionBuilder.or(qGuestbook.content.contains(keyword));
+        }
+        if (type.contains("w")) { // writer
+            conditionBuilder.or(qGuestbook.writer.contains(keyword));
+        }
+
+        // 조건 통함
+        booleanBuilder.and(conditionBuilder);
+
+        return booleanBuilder;
+    }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+        BooleanBuilder booleanBuilder = getSearch(requestDTO); // 검색 조건 처리
+        Page<Guestbook> result = repository.findAll(booleanBuilder, pageable);
+        Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
+
+        return new PageResultDTO<>(result, fn);
+
     }
 }
